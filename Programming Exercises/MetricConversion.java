b20ae9f0d4cf1bc62a78b2c7b73e2489950af2f7
two name <https://github.com/carlton435>
2021-12-19 16:20:14 +0800

    finish

---
+++ Programming Exercises/MetricConversion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MetricConversion
+{
+    public static void main (String args[])
+    {
+        int aValue;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer >> ");
+        aValue = input.nextInt();
+        convertToCentimeters(aValue);
+    }
+
+    public static void convertToCentimeters(int inches)
+    {
+        double centimeters;
+        final double CENT_FACTOR = 2.54;
+        centimeters = inches * CENT_FACTOR;
+        System.out.println(inches + " inches is " + centimeters + " centimeters");
+    }
+}
